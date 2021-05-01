ea558e5695a1ea2e13147a7242b961bfb1176d2a
karim kamel <karim.kamel@live.com>
2021-05-01 11:56:45 +0200

    add secure cookie settings for production

--- config/session.js
+++ config/session.js
@@ -4,7 +4,7 @@ mongoStore = MongoStore.create({
 });
 
 function configSession(session) {
-  return session({
+  session({
     secret: process.env.SESSION_SECRET,
     resave: false,
 
@@ -12,10 +12,19 @@ function configSession(session) {
     cookie: {
       secure: false,
       httpOnly: true,
+      store: MongoStore.create({
+        mongoUrl: process.env.MONGO_CONNECTION_STRING,
+      }),
       // maxAge: 1000 * 60 * 60 * 24 * 365,
     },
-
-    store: MongoStore.create({ mongoUrl: process.env.MONGO_CONNECTION_STRING }),
   });
+  console.log(process.env.Node_ENV);
+  console.log("#########################");
+  if (process.env.NODE_ENV === "production") {
+    console.log("secure cookie switched on");
+    sess.cookie.secure = true;
+  }
+
+  return session;
 }
 module.exports = configSession;
