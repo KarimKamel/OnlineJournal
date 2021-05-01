5929377426f41d043f6cdfbdb7ce472912e8f40b
karim kamel <karim.kamel@live.com>
2021-05-01 18:33:20 +0200

    'pre-deploy'

--- config/session.js
+++ config/session.js
@@ -10,13 +10,13 @@ function configSession() {
     resave: true,
 
     saveUninitialized: false,
-    cookie: {
-      secure: false,
-      httpOnly: false,
-      sameSite: false,
+    // cookie: {
+    //   secure: false,
+    //   httpOnly: false,
+    //   sameSite: false,
 
-      maxAge: 1000 * 60 * 60 * 24 * 365,
-    },
+    //   maxAge: 1000 * 60 * 60 * 24 * 365,
+    // },
     store: MongoStore.create({
       mongoUrl: process.env.MONGO_CONNECTION_STRING,
     }),
